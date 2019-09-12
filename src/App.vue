2e286d22834cf064e4f71f70aebfd8412933d00d
TatagW <tatagwibowo95@gmail.com>
2019-09-12 18:15:36 +0700

    skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
-  <h1>{{message}}</h1>
+    <div>
+        <h1>{{test}}</h1>
+        <login @tambaho="tambahO"></login>
+    <div>
 </template>
 
 <script>
+
 export default {
     data: function(){
         return {
             test: "halooo"
         }
+    },
+    methods: {
+        tambahO(){
+            this.test+= 'o'
+        }
+    },
+    components : {
+        login : loginForm
     }
 }
 </script>
 
 <style>
-    .h1 {
+    h1 {
         color: red;
     }
 </style>
