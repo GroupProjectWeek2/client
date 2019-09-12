f5d8f5524dddd424e8d9a7e6f17109b70e78bbe9
TatagW <tatagwibowo95@gmail.com>
2019-09-12 20:48:54 +0700

    login form

--- src/components/loginFrom.vue
+++ src/components/loginFrom.vue
@@ -1,22 +1,61 @@
 <template>
-    <form action="">
-        <input v-model="email" type="email" placeholder="Email">
-        <input v-model="password" type="password" placeholder="Password">
-        <button @click.prevent="login" type="submit">Sign in</button>
-    </form>
+    <div>
+        <h1>Sign in.</h1>
+        <p v-bind:style="{visibility: errorShow}">{{error}}</p>
+        <form action="">
+            <input v-model="email" type="email" placeholder="Email">
+            <input v-model="password" type="password" placeholder="Password">
+            <button v-if="loading === false" @click.prevent="login" type="submit">Sign in</button>
+            <button v-else type="submit"><i class="fas fa-spinner fa-pulse"></i></button>
+            
+        </form>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        
+    </div>
 </template>
 
 <script>
+
+import axios from 'axios'
 export default {
     data: function(){
         return {
             email: "",
-            password: ""
+            password: "",
+            error: "",
+            errorShow: "hidden",
+            loading: false,
+            baseUrl: "http://34.87.72.235"
         }
     },
     methods: {
         login(){
-            console.log(this.email, this.password)
+            this.loading = true
+            axios({
+                method: 'post',
+                url: `${this.baseUrl}/users/login`,
+                data: {
+                    email: this.email,
+                    password: this.password
+                }
+            })
+            .then(response =>{
+                localStorage('token', response.data.token)
+                this.loading = false
+
+            })
+            .catch(err => {
+                this.loading = false
+                
+                if(err.response){
+                    this.error = err.response.data.message
+                }else if(err.request){
+                    this.errorMessage = `500 Internal Server Error`
+                }else {
+                    console.log(err.message)
+                }
+                this.errorShow = "visible"
+            })
         }
     }
 }
@@ -26,6 +65,45 @@ export default {
     form{
         display: flex;
         flex-direction: column;
-        width: 35vw;
+    }
+    p {
+        font-family: 'Roboto', sans-serif;
+        color: red;
+        padding: 5px;
+        height: 8vh;
+        word-wrap:break-word;
+        width: 100%;
+        margin: 0px;
+    }
+    input, button{
+        height: 5vh;
+        box-sizing: border-box;
+        border-radius: 5px;
+        outline: none;
+        border: 1px solid #cccccc;
+        font-size: 1rem;
+        margin-bottom: 1.5vh;
+    }
+    input {
+        padding-left: 1vw;
+    }
+    button {
+        background-color: black;
+        color: white;
+        font-size: 1.3rem;
+        margin-top: 3vh;
+        margin-bottom: 3vh;
+        height: 6vh;
+    }
+    button:hover {
+        background-color: #252625;
+    }
+    .g-signin2 {
+        display: flex;
+        justify-content: space-evenly;
+    }
+    div{
+        font-family: 'Roboto', sans-serif; 
+        width: 25vw;
     }
 </style>
