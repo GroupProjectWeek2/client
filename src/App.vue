db9be387f136726e632853e02644404b9589c211
TatagW <tatagwibowo95@gmail.com>
2019-09-12 19:06:11 +0700

    starting loginform

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <h1>{{message}}</h1>
+    <div>
+        <firstpage v-if="page === 1"></firstpage>
+        <secondpage v-else-if="page === 2"></secondpage>
+    </div>
 </template>
 
 <script>
+import firstpage from './components/firstPage'
+import secondpage from './components/secondPage';
 export default {
     data: function(){
         return {
-            test: "halooo"
+            page: 1
         }
+    },
+    components: {
+        firstpage,
+        secondpage
     }
+
 }
 </script>
 
 <style>
-    .h1 {
-        color: red;
-    }
+    
 </style>
---
+++ src/components/loginFrom.vue
@@ -0,0 +1,31 @@
+<template>
+    <form action="">
+        <input v-model="email" type="email" placeholder="Email">
+        <input v-model="password" type="password" placeholder="Password">
+        <button @click.prevent="login" type="submit">Sign in</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        login(){
+            console.log(this.email, this.password)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form{
+        display: flex;
+        flex-direction: column;
+        width: 35vw;
+    }
+</style>
